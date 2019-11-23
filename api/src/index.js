274f8f0c30cbdc4bcea9fa2b641854046d2d2c30
Dylan Gore <hello@dylangore.ie>
2019-11-23 15:52:09 +0000

    Added ESLint and Prettier config to API and updated packages

--- api/src/index.js
+++ api/src/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get('/', function(req, res) {
+    res.send('Project Manager API');
+});
+
 app.listen(port, function() {
     console.log('Example app listening on port ' + port);
 });
